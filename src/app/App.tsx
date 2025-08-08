64c595f799f1e4fea3d2e234e14b28ea6c7025cc
Valentin <symrak1996@gmail.com>
2025-08-08 22:26:43 +0300

    architecture setup for app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
 	const [count, setCount] = useState(0)
 
+	console.log({ count })
 	return (
 		<>
 			<div>
