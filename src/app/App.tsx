099660d2a12ac7d9496ffc6d0a353dd5a4b94064
Valentin <symrak1996@gmail.com>
2025-08-17 19:19:16 +0300

    change Theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Button from "@mui/material/Button";
+import AppBar from "./AppBar";
+import ToggleColorMode from "./colorMode";
 
 const App = () => {
-	const [count, setCount] = useState(0)
-
-	console.log({ count })
+	const [count, setCount] = useState(0);
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
+			<AppBar />
+			<ToggleColorMode />
+			<div
+				style={{
+					position: "absolute",
+					zIndex: 6,
+				}}
+			>
+				<div>
+					<a href="https://vite.dev" target="_blank">
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+					</a>
+					<a href="https://react.dev" target="_blank">
+						<img src={reactLogo} className="logo react" alt="React logo" />
+					</a>
+				</div>
+				<h1>Vite + React</h1>
+				<div className="card">
+					<Button variant={"contained"} onClick={() => setCount((count) => count + 1)}>
+						count is {count}
+					</Button>
+					<p>
+						Edit <code>src/App.tsx</code> and save to test HMR
+					</p>
+				</div>
+				<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
-	)
-}
+	);
+};
 
-export default App
+export default App;
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById("root")!).render(
+	<>
+		<CssBaseline />
+		<App />
+	</>
+);
