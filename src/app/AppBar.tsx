d6ad13d0ec4e521f61f32161c73867486603a7b6
Valentin <symrak1996@gmail.com>
2025-08-24 16:10:47 +0300

    изменение темы

--- src/app/AppBar.tsx
+++ src/app/AppBar.tsx
@@ -5,28 +5,57 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Avatar, Tooltip } from "@mui/material";
+import { Avatar, Stack, Tooltip } from "@mui/material";
+
+import RadioGroup from "@mui/material/RadioGroup";
+import Radio from "@mui/material/Radio";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { useColorScheme } from "@mui/material/styles";
 
 const ButtonAppBar = () => {
+	const { mode, setMode } = useColorScheme();
+	if (!mode) {
+		return null;
+	}
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Toolbar>
+			<AppBar sx={{ position: "fixed", minHeight: 30 }}>
+				<Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
 					<IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-						<MenuIcon />
+						<div style={{ display: "flex", gap: 15 }}>
+							<MenuIcon />
+							<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+								Todos
+							</Typography>
+							<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+								About
+							</Typography>
+						</div>
 					</IconButton>
-					<div style={{ display: "flex" }}>
-						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-							Todos
-						</Typography>
-						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-							About
-						</Typography>
+
+					<div style={{ display: "flex", alignItems: "center" }}>
+						<FormControl>
+							<RadioGroup
+								aria-labelledby="demo-theme-toggle"
+								name="theme-toggle"
+								row
+								value={mode}
+								onChange={(event) => {
+									setMode(event.target.value as "light" | "dark");
+									console.log(mode);
+								}}
+							>
+								<FormControlLabel value="light" control={<Radio />} label="Light" />
+								<FormControlLabel value="dark" control={<Radio />} label="Dark" />
+							</RadioGroup>
+						</FormControl>
+
+						<Button color="inherit">Login</Button>
+						<Tooltip title="User">
+							<Avatar src={""} />
+						</Tooltip>
 					</div>
-					<Button color="inherit">Login</Button>
-					<Tooltip title="User">
-						<Avatar src={""} />
-					</Tooltip>
 				</Toolbar>
 			</AppBar>
 		</Box>
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,10 +2,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+// // Значение темы по умолчанию
+const darkTheme = createTheme({
+	colorSchemes: {
+		dark: true,
+	},
+
+});
 
 createRoot(document.getElementById("root")!).render(
 	<>
-		<CssBaseline />
-		<App />
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
 	</>
 );
