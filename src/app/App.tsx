f93e39a7cd9f6d862e9a296691a019d0f38dec12
Valentin <symrak1996@gmail.com>
2025-09-13 16:55:36 +0300

    Создал формочку логинизации

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,38 +1,160 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import Button from "@mui/material/Button";
 import AppBar from "./AppBar";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import { Button, Stack } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { useState, type SyntheticEvent } from "react";
+import SaveIcon from "@mui/icons-material/Save";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
 const App = () => {
-	const [count, setCount] = useState(0);
+	// Хранилище для логина
+	const [username, setUserName] = useState("");
+	// Хранилище для пароля
+	const [userpassword, setPassword] = useState("");
+
+	// Имитация загрузки на сервер(с помощью флага)
+	const [loading, setLoading] = useState(false);
+
+	// Флаг для изменения формы логинизации или регистрация
+	const [loginFormName, setloginFormName] = useState("login");
+
+	// Сохраняет логин в переменную username
+	const handleUserNameChange = (e: SyntheticEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		setUserName(e.currentTarget.value);
+	};
+	const handlePasswordChange = (e: SyntheticEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		setPassword(e.currentTarget.value);
+	};
+
+	const handleLoggin = () => {
+		setLoading(true);
+		// Имитация отмены закгрузки на сервер
+		setTimeout(() => {
+			setLoading(false);
+		}, 2000);
+	};
+	// Функция для измения значения флага
+	const handleChange = (_event: React.MouseEvent<HTMLElement>, newAlignment: string) => {
+		setloginFormName(newAlignment);
+	};
+
 	return (
 		<>
 			<AppBar />
-			<div style={{ marginTop: 80 }}>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<Button variant={"contained"} onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</Button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-			
+			<div style={{ marginTop: "100px" }}></div>
+
+			<Container maxWidth="sm">
+				<ToggleButtonGroup
+					disabled={loading}
+					size="small"
+					color="primary"
+					value={loginFormName}
+					exclusive
+					onChange={handleChange}
+					aria-label="Platform"
+					fullWidth
+					sx={{ marginBottom: 2 }}
+				>
+					<ToggleButton value="login">Login</ToggleButton>
+					<ToggleButton value="register">Register</ToggleButton>
+				</ToggleButtonGroup>
+				{loginFormName === "login" ? (
+					<Stack spacing={2}>
+						<TextField
+							// Пока идёт загрузка(loading), disabled запрещает ввод
+							disabled={loading}
+							// value нужен для синхронизации того что мы напечатали с пермееной username, т.к. пока всё хранится в input
+							value={username}
+							onChange={handleUserNameChange}
+							size="small"
+							label="email"
+							variant="filled"
+							slotProps={{
+								input: {
+									startAdornment: (
+										<InputAdornment position="start">
+											<AccountCircle />
+										</InputAdornment>
+									),
+								},
+							}}
+						/>
+						<TextField
+							disabled={loading}
+							value={userpassword}
+							onChange={handlePasswordChange}
+							size="small"
+							label="password"
+							variant="filled"
+							type="password"
+							slotProps={{
+								input: {
+									startAdornment: (
+										<InputAdornment position="start">
+											<AccountCircle />
+										</InputAdornment>
+									),
+								},
+							}}
+						/>
+						<Button onClick={handleLoggin} variant="contained" loading={loading} loadingPosition="start">
+							{loading ? "Loading" : "Login"}
+						</Button>
+					</Stack>
+				) : (
+					<Stack spacing={2}>
+						<TextField
+							// Пока идёт загрузка(loading), disabled запрещает ввод
+							disabled={loading}
+							// value нужен для синхронизации того что мы напечатали с пермееной username, т.к. пока всё хранится в input
+							value={username}
+							onChange={handleUserNameChange}
+							size="small"
+							label="email"
+							variant="filled"
+							slotProps={{
+								input: {
+									startAdornment: (
+										<InputAdornment position="start">
+											<AccountCircle />
+										</InputAdornment>
+									),
+								},
+							}}
+						/>
+						<TextField
+							disabled={loading}
+							value={userpassword}
+							onChange={handlePasswordChange}
+							size="small"
+							label="password"
+							variant="filled"
+							type="password"
+							slotProps={{
+								input: {
+									startAdornment: (
+										<InputAdornment position="start">
+											<AccountCircle />
+										</InputAdornment>
+									),
+								},
+							}}
+						/>
+						<Button onClick={handleLoggin} variant="contained" loading={loading} loadingPosition="start">
+							{loading ? "Loading" : "Register"}
+						</Button>
+					</Stack>
+				)}
+			</Container>
 		</>
 	);
 };
